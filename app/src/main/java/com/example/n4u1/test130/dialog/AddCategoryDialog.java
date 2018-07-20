b6768b1ec60fcc86c995ef719bf5aeb2fbc7a068
n4u1 <40189757+n4u1@users.noreply.github.com>
2018-07-20 17:59:56 +0900

    20180720_1800

--- app/src/main/java/com/example/n4u1/test130/dialog/AddCategoryDialog.java
+++ app/src/main/java/com/example/n4u1/test130/dialog/AddCategoryDialog.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -13,31 +14,50 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.n4u1.test130.R;
 
 
 public class AddCategoryDialog extends DialogFragment {
-
-
-
-
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+
         AlertDialog.Builder mBuilder = new AlertDialog.Builder(getActivity());
         LayoutInflater mLayoutInflater = getActivity().getLayoutInflater();
         View view = mLayoutInflater.inflate(R.layout.activity_add_category_dialog, null);
         mBuilder.setView(mLayoutInflater.inflate(R.layout.activity_add_category_dialog, null));
-        mBuilder.setTitle("카테고리 선택");
 
+        final CheckBox checkBox0 = view.getRootView().findViewById(R.id.checkBox_0);
+        final CheckBox checkBox1 = view.getRootView().findViewById(R.id.checkBox_1);
+        final CheckBox checkBox2 = view.getRootView().findViewById(R.id.checkBox_2);
+        final CheckBox checkBox3 = view.getRootView().findViewById(R.id.checkBox_3);
+        final CheckBox checkBox4 = view.getRootView().findViewById(R.id.checkBox_4);
+        final CheckBox checkBox5 = view.getRootView().findViewById(R.id.checkBox_5);
+        final CheckBox checkBox6 = view.getRootView().findViewById(R.id.checkBox_6);
+        final CheckBox checkBox7 = view.getRootView().findViewById(R.id.checkBox_7);
+        final CheckBox checkBox8 = view.getRootView().findViewById(R.id.checkBox_8);
+        final CheckBox checkBox9 = view.getRootView().findViewById(R.id.checkBox_9);
+        final CheckBox checkBox10 = view.getRootView().findViewById(R.id.checkBox_10);
+        final CheckBox checkBox11 = view.getRootView().findViewById(R.id.checkBox_11);
+
+
+
+        mBuilder.setTitle("카테고리 선택aa");
         mBuilder.setView(view)
                 .setPositiveButton("확인",
                         new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int id) {
+                                for (int i = 0; i < 13; i++) {
+
+
+                                }
+
 
                             }
                         }).setNegativeButton("취소", null);
--- app/src/main/java/com/example/n4u1/test130/views/UserContentsUploadActivity.java
+++ app/src/main/java/com/example/n4u1/test130/views/UserContentsUploadActivity.java
@@ -1,10 +1,12 @@
 package com.example.n4u1.test130.views;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 import com.example.n4u1.test130.R;
 import com.example.n4u1.test130.dialog.AddCategoryDialog;
@@ -20,6 +22,7 @@ public class UserContentsUploadActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_contents_upload);
 
+        CheckBox checkBox0 = findViewById(R.id.checkBox_0);
 
 
         findViewById(R.id.button_addCategory).setOnClickListener(new View.OnClickListener() {
@@ -31,5 +34,12 @@ public class UserContentsUploadActivity extends AppCompatActivity {
 
             }
         });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
