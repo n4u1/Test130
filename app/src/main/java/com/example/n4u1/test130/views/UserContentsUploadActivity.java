7c57cf2785540ad3cdbbab9b0ea0b8de768a2721
n4u1 <lkj840211@gmail.com>
2018-07-24 03:15:34 +0900

    20180724_0315

--- app/src/main/java/com/example/n4u1/test130/views/UserContentsUploadActivity.java
+++ app/src/main/java/com/example/n4u1/test130/views/UserContentsUploadActivity.java
@@ -6,37 +6,44 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.n4u1.test130.R;
-import com.example.n4u1.test130.dialog.AddCategoryDialog;
+
 import com.example.n4u1.test130.dialog.ContentChoiceDialog;
 
 import java.util.ArrayList;
 
 public class UserContentsUploadActivity extends AppCompatActivity implements ContentChoiceDialog.ContentChoiceDialogListener {
 
-    private AddCategoryDialog mAddCategoryDialog;
-    private Button mRightButton;
-    private Button mLeftButton;
 
-    @Override
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_contents_upload);
 
+//
+//        findViewById(R.id.button_addCategory).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                ContentChoiceDialog contentChoiceDialog = new ContentChoiceDialog();
+//                contentChoiceDialog.show(getSupportFragmentManager(), "Category Choice");
+//
+//            }
+//        });
 
-        findViewById(R.id.button_addCategory).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ContentChoiceDialog contentChoiceDialog = new ContentChoiceDialog();
-                contentChoiceDialog.show(getSupportFragmentManager(), "Category Choice");
-
-            }
-        });
+        EditText editText = findViewById(R.id.editText_addCategory);
+            editText.setFocusable(false);
+            editText.setClickable(false);
+            editText.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ContentChoiceDialog contentChoiceDialog = new ContentChoiceDialog();
+                    contentChoiceDialog.show(getSupportFragmentManager(), "Category Choice");
+                }
+            });
 
 
     }
@@ -45,27 +52,12 @@ public class UserContentsUploadActivity extends AppCompatActivity implements Con
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 //        super.onActivityResult(requestCode, resultCode, data);
 
-        if (resultCode == 3959) {
-            TextView textView = findViewById(R.id.textView_choiceCategory);
-            Intent intent = getIntent();
-//            Bundle mArgs = new Bundle();
-//
-            String stringResult = "";
-            String[] myStrings = intent.getStringArrayExtra("Strings");
-            for (int i = 0; i < 12; i++) {
-                if (!myStrings[i].equals("")) {
-                    stringResult = stringResult + myStrings[i];
-                }
-            }
-            textView.setText(stringResult);
-
-        }
     }
 
 
     @Override
     public void onDialogPositiveClick(ArrayList arrayList) {
-        TextView textView = findViewById(R.id.textView_choiceCategory);
+        TextView textView = findViewById(R.id.editText_addCategory);
         String string = arrayList.toString();
         String resultString = string.replace("[", "").replace("]","");
 
