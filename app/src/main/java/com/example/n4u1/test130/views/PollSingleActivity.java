eb5574cae0294a4b2292e22f11a50618e23a1ce5
n4u1 <lkj840211@gmail.com>
2018-08-14 07:44:36 +0900

    20180814_0743_pollActivity_imgChecking

--- app/src/main/java/com/example/n4u1/test130/views/PollSingleActivity.java
+++ app/src/main/java/com/example/n4u1/test130/views/PollSingleActivity.java
@@ -19,7 +19,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 
-public class PollSingleActivity extends AppCompatActivity {
+public class PollSingleActivity extends AppCompatActivity implements View.OnClickListener{
 
     DatabaseReference mDatabaseReference;
 
@@ -30,7 +30,11 @@ public class PollSingleActivity extends AppCompatActivity {
             pollActivity_imageView_userAddContent_5, pollActivity_imageView_userAddContent_6,
             pollActivity_imageView_userAddContent_7, pollActivity_imageView_userAddContent_8,
             pollActivity_imageView_userAddContent_9, pollActivity_imageView_userAddContent_10;
-    ImageView pollActivity_imageView_userAddContent_check_1;
+    ImageView pollActivity_imageView_userAddContent_check_1, pollActivity_imageView_userAddContent_check_2,
+            pollActivity_imageView_userAddContent_check_3, pollActivity_imageView_userAddContent_check_4,
+            pollActivity_imageView_userAddContent_check_5, pollActivity_imageView_userAddContent_check_6,
+            pollActivity_imageView_userAddContent_check_7, pollActivity_imageView_userAddContent_check_8,
+            pollActivity_imageView_userAddContent_check_9, pollActivity_imageView_userAddContent_check_10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,8 +71,28 @@ public class PollSingleActivity extends AppCompatActivity {
         pollActivity_imageView_userAddContent_8 = findViewById(R.id.pollActivity_imageView_userAddContent_8);
         pollActivity_imageView_userAddContent_9 = findViewById(R.id.pollActivity_imageView_userAddContent_9);
         pollActivity_imageView_userAddContent_10 = findViewById(R.id.pollActivity_imageView_userAddContent_10);
-//        pollActivity_imageView_userAddContent_check_1 = findViewById(R.id.pollActivity_imageView_userAddContent_check_1);
+        pollActivity_imageView_userAddContent_check_1 = findViewById(R.id.pollActivity_imageView_userAddContent_check_1);
+        pollActivity_imageView_userAddContent_check_2 = findViewById(R.id.pollActivity_imageView_userAddContent_check_2);
+        pollActivity_imageView_userAddContent_check_3 = findViewById(R.id.pollActivity_imageView_userAddContent_check_3);
+        pollActivity_imageView_userAddContent_check_4 = findViewById(R.id.pollActivity_imageView_userAddContent_check_4);
+        pollActivity_imageView_userAddContent_check_5 = findViewById(R.id.pollActivity_imageView_userAddContent_check_5);
+        pollActivity_imageView_userAddContent_check_6 = findViewById(R.id.pollActivity_imageView_userAddContent_check_6);
+        pollActivity_imageView_userAddContent_check_7 = findViewById(R.id.pollActivity_imageView_userAddContent_check_7);
+        pollActivity_imageView_userAddContent_check_8 = findViewById(R.id.pollActivity_imageView_userAddContent_check_8);
+        pollActivity_imageView_userAddContent_check_9 = findViewById(R.id.pollActivity_imageView_userAddContent_check_9);
+        pollActivity_imageView_userAddContent_check_10 = findViewById(R.id.pollActivity_imageView_userAddContent_check_10);
 
+
+        pollActivity_imageView_userAddContent_1.setOnClickListener(this);
+        pollActivity_imageView_userAddContent_2.setOnClickListener(this);
+        pollActivity_imageView_userAddContent_3.setOnClickListener(this);
+        pollActivity_imageView_userAddContent_4.setOnClickListener(this);
+        pollActivity_imageView_userAddContent_5.setOnClickListener(this);
+        pollActivity_imageView_userAddContent_6.setOnClickListener(this);
+        pollActivity_imageView_userAddContent_7.setOnClickListener(this);
+        pollActivity_imageView_userAddContent_8.setOnClickListener(this);
+        pollActivity_imageView_userAddContent_9.setOnClickListener(this);
+        pollActivity_imageView_userAddContent_10.setOnClickListener(this);
 
 
 
@@ -231,11 +255,232 @@ public class PollSingleActivity extends AppCompatActivity {
         });
     }
 
-    public void checking_img_1() {
-        pollActivity_imageView_userAddContent_1.setAlpha(0.3f);
-        Glide.with(this)
-                .load(R.drawable.ic_outline_check_24px)
-                .into(pollActivity_imageView_userAddContent_check_1)
-                .getView();
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.pollActivity_imageView_userAddContent_1 :
+                checking_img_1();
+                checking_img_2_rt();
+                checking_img_3_rt();
+                checking_img_4_rt();
+                checking_img_5_rt();
+                checking_img_6_rt();
+                checking_img_7_rt();
+                checking_img_8_rt();
+                checking_img_9_rt();
+                checking_img_10_rt();
+                break;
+            case R.id.pollActivity_imageView_userAddContent_2 :
+                checking_img_1_rt();
+                checking_img_2();
+                checking_img_3_rt();
+                checking_img_4_rt();
+                checking_img_5_rt();
+                checking_img_6_rt();
+                checking_img_7_rt();
+                checking_img_8_rt();
+                checking_img_9_rt();
+                checking_img_10_rt();
+                break;
+            case R.id.pollActivity_imageView_userAddContent_3 :
+                checking_img_1_rt();
+                checking_img_2_rt();
+                checking_img_3();
+                checking_img_4_rt();
+                checking_img_5_rt();
+                checking_img_6_rt();
+                checking_img_7_rt();
+                checking_img_8_rt();
+                checking_img_9_rt();
+                checking_img_10_rt();
+                break;
+            case R.id.pollActivity_imageView_userAddContent_4 :
+                checking_img_1_rt();
+                checking_img_2_rt();
+                checking_img_3_rt();
+                checking_img_4();
+                checking_img_5_rt();
+                checking_img_6_rt();
+                checking_img_7_rt();
+                checking_img_8_rt();
+                checking_img_9_rt();
+                checking_img_10_rt();
+                break;
+            case R.id.pollActivity_imageView_userAddContent_5 :
+                checking_img_1_rt();
+                checking_img_2_rt();
+                checking_img_3_rt();
+                checking_img_4_rt();
+                checking_img_5();
+                checking_img_6_rt();
+                checking_img_7_rt();
+                checking_img_8_rt();
+                checking_img_9_rt();
+                checking_img_10_rt();
+                break;
+            case R.id.pollActivity_imageView_userAddContent_6 :
+                checking_img_1_rt();
+                checking_img_2_rt();
+                checking_img_3_rt();
+                checking_img_4_rt();
+                checking_img_5_rt();
+                checking_img_6();
+                checking_img_7_rt();
+                checking_img_8_rt();
+                checking_img_9_rt();
+                checking_img_10_rt();
+                break;
+            case R.id.pollActivity_imageView_userAddContent_7 :
+                checking_img_1_rt();
+                checking_img_2_rt();
+                checking_img_3_rt();
+                checking_img_4_rt();
+                checking_img_5_rt();
+                checking_img_6_rt();
+                checking_img_7();
+                checking_img_8_rt();
+                checking_img_9_rt();
+                checking_img_10_rt();
+                break;
+            case R.id.pollActivity_imageView_userAddContent_8 :
+                checking_img_1_rt();
+                checking_img_2_rt();
+                checking_img_3_rt();
+                checking_img_4_rt();
+                checking_img_5_rt();
+                checking_img_6_rt();
+                checking_img_7_rt();
+                checking_img_8();
+                checking_img_9_rt();
+                checking_img_10_rt();
+                break;
+            case R.id.pollActivity_imageView_userAddContent_9 :
+                checking_img_1_rt();
+                checking_img_2_rt();
+                checking_img_3_rt();
+                checking_img_4_rt();
+                checking_img_5_rt();
+                checking_img_6_rt();
+                checking_img_7_rt();
+                checking_img_8_rt();
+                checking_img_9();
+                checking_img_10_rt();
+                break;
+            case R.id.pollActivity_imageView_userAddContent_10 :
+                checking_img_1_rt();
+                checking_img_2_rt();
+                checking_img_3_rt();
+                checking_img_4_rt();
+                checking_img_5_rt();
+                checking_img_6_rt();
+                checking_img_7_rt();
+                checking_img_8_rt();
+                checking_img_9_rt();
+                checking_img_10();
+                break;
+        }
+
     }
+
+    public void checking_img_1() {
+        pollActivity_imageView_userAddContent_1.setAlpha(0.7f);
+        pollActivity_imageView_userAddContent_check_1.setVisibility(View.VISIBLE);
+        Glide.with(this).load(R.drawable.ic_outline_check_24px).into(pollActivity_imageView_userAddContent_check_1).getView();
+    }
+    public void checking_img_1_rt() {
+        pollActivity_imageView_userAddContent_1.setAlpha(1.0f);
+        pollActivity_imageView_userAddContent_check_1.setVisibility(View.GONE);
+    }
+
+    public void checking_img_2() {
+        pollActivity_imageView_userAddContent_2.setAlpha(0.7f);
+        pollActivity_imageView_userAddContent_check_2.setVisibility(View.VISIBLE);
+        Glide.with(this).load(R.drawable.ic_outline_check_24px).into(pollActivity_imageView_userAddContent_check_2).getView();
+    }
+    public void checking_img_2_rt() {
+        pollActivity_imageView_userAddContent_2.setAlpha(1.0f);
+        pollActivity_imageView_userAddContent_check_2.setVisibility(View.GONE);
+    }
+
+    public void checking_img_3() {
+        pollActivity_imageView_userAddContent_3.setAlpha(0.7f);
+        pollActivity_imageView_userAddContent_check_3.setVisibility(View.VISIBLE);
+        Glide.with(this).load(R.drawable.ic_outline_check_24px).into(pollActivity_imageView_userAddContent_check_3).getView();
+    }
+    public void checking_img_3_rt() {
+        pollActivity_imageView_userAddContent_3.setAlpha(1.0f);
+        pollActivity_imageView_userAddContent_check_3.setVisibility(View.GONE);
+    }
+
+    public void checking_img_4() {
+        pollActivity_imageView_userAddContent_4.setAlpha(0.7f);
+        pollActivity_imageView_userAddContent_check_4.setVisibility(View.VISIBLE);
+        Glide.with(this).load(R.drawable.ic_outline_check_24px).into(pollActivity_imageView_userAddContent_check_4).getView();
+    }
+    public void checking_img_4_rt() {
+        pollActivity_imageView_userAddContent_4.setAlpha(1.0f);
+        pollActivity_imageView_userAddContent_check_4.setVisibility(View.GONE);
+    }
+
+    public void checking_img_5() {
+        pollActivity_imageView_userAddContent_5.setAlpha(0.7f);
+        pollActivity_imageView_userAddContent_check_5.setVisibility(View.VISIBLE);
+        Glide.with(this).load(R.drawable.ic_outline_check_24px).into(pollActivity_imageView_userAddContent_check_5).getView();
+    }
+    public void checking_img_5_rt() {
+        pollActivity_imageView_userAddContent_5.setAlpha(1.0f);
+        pollActivity_imageView_userAddContent_check_5.setVisibility(View.GONE);
+    }
+
+    public void checking_img_6() {
+        pollActivity_imageView_userAddContent_6.setAlpha(0.7f);
+        pollActivity_imageView_userAddContent_check_6.setVisibility(View.VISIBLE);
+        Glide.with(this).load(R.drawable.ic_outline_check_24px).into(pollActivity_imageView_userAddContent_check_6).getView();
+    }
+    public void checking_img_6_rt() {
+        pollActivity_imageView_userAddContent_6.setAlpha(1.0f);
+        pollActivity_imageView_userAddContent_check_6.setVisibility(View.GONE);
+    }
+
+    public void checking_img_7() {
+        pollActivity_imageView_userAddContent_7.setAlpha(0.7f);
+        pollActivity_imageView_userAddContent_check_7.setVisibility(View.VISIBLE);
+        Glide.with(this).load(R.drawable.ic_outline_check_24px).into(pollActivity_imageView_userAddContent_check_7).getView();
+    }
+    public void checking_img_7_rt() {
+        pollActivity_imageView_userAddContent_7.setAlpha(1.0f);
+        pollActivity_imageView_userAddContent_check_7.setVisibility(View.GONE);
+    }
+
+    public void checking_img_8() {
+        pollActivity_imageView_userAddContent_8.setAlpha(0.7f);
+        pollActivity_imageView_userAddContent_check_8.setVisibility(View.VISIBLE);
+        Glide.with(this).load(R.drawable.ic_outline_check_24px).into(pollActivity_imageView_userAddContent_check_8).getView();
+    }
+    public void checking_img_8_rt() {
+        pollActivity_imageView_userAddContent_8.setAlpha(1.0f);
+        pollActivity_imageView_userAddContent_check_8.setVisibility(View.GONE);
+    }
+
+    public void checking_img_9() {
+        pollActivity_imageView_userAddContent_9.setAlpha(0.7f);
+        pollActivity_imageView_userAddContent_check_9.setVisibility(View.VISIBLE);
+        Glide.with(this).load(R.drawable.ic_outline_check_24px).into(pollActivity_imageView_userAddContent_check_9).getView();
+    }
+    public void checking_img_9_rt() {
+        pollActivity_imageView_userAddContent_9.setAlpha(1.0f);
+        pollActivity_imageView_userAddContent_check_9.setVisibility(View.GONE);
+    }
+
+    public void checking_img_10() {
+        pollActivity_imageView_userAddContent_10.setAlpha(0.7f);
+        pollActivity_imageView_userAddContent_check_10.setVisibility(View.VISIBLE);
+        Glide.with(this).load(R.drawable.ic_outline_check_24px).into(pollActivity_imageView_userAddContent_check_10).getView();
+    }
+    public void checking_img_10_rt() {
+        pollActivity_imageView_userAddContent_10.setAlpha(1.0f);
+        pollActivity_imageView_userAddContent_check_10.setVisibility(View.GONE);
+    }
+
 }
