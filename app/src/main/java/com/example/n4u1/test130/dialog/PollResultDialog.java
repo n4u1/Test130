4e06b2d5d7183ee7a74571df1e223c69fecaa4cf
n4u1 <40189757+n4u1@users.noreply.github.com>
2018-08-31 18:00:19 +0900

    20180831_1800_StatisticsParsing

--- app/src/main/java/com/example/n4u1/test130/dialog/PollResultDialog.java
+++ app/src/main/java/com/example/n4u1/test130/dialog/PollResultDialog.java
@@ -53,8 +53,6 @@ public class PollResultDialog extends DialogFragment {
     private DatabaseReference mDatabaseReference;
 
     int contentHit;
-    int male = 0;
-    int female = 0;
     int imageN;
     int currentPick;
     String contentKey;
@@ -62,11 +60,9 @@ public class PollResultDialog extends DialogFragment {
     String genderRange = "전 체";
     String ageRange = "전 체";
     AppCompatSpinner pollResultDialog_spinner_age, pollResultDialog_spinner_gender;
-    
+
     List<String> ageRangeList = new ArrayList<>();
     List<String> genderList = new ArrayList<>();
-
-
 
 
     @Nullable
@@ -77,34 +73,43 @@ public class PollResultDialog extends DialogFragment {
         pollResultDialog_spinner_age = view.findViewById(R.id.pollResultDialog_spinner_age);
         pollResultDialog_spinner_gender = view.findViewById(R.id.pollResultDialog_spinner_gender);
 
-          genderList.add("전 체");
-          genderList.add("여자");
-          genderList.add("남자");
+        genderList.add("전 체");
+        genderList.add("여 자");
+        genderList.add("남 자");
         ageRangeList.add("전 체");
+        ageRangeList.add("10대 전체 (10 ~ 19)");
         ageRangeList.add("10대 초반 (10 ~ 12)");
         ageRangeList.add("10대 중반 (13 ~ 16)");
         ageRangeList.add("10대 후반 (17 ~ 19)");
+        ageRangeList.add("20대 전체 (20 ~ 29)");
         ageRangeList.add("20대 초반 (20 ~ 22)");
         ageRangeList.add("20대 중반 (23 ~ 26)");
         ageRangeList.add("20대 후반 (27 ~ 20)");
+        ageRangeList.add("30대 전체 (30 ~ 39)");
         ageRangeList.add("30대 초반 (30 ~ 32)");
         ageRangeList.add("30대 중반 (33 ~ 36)");
         ageRangeList.add("30대 후반 (37 ~ 39)");
+        ageRangeList.add("40대 전체 (40 ~ 49)");
         ageRangeList.add("40대 초반 (40 ~ 42)");
         ageRangeList.add("40대 중반 (43 ~ 46)");
         ageRangeList.add("40대 후반 (47 ~ 40)");
+        ageRangeList.add("50대 전체 (50 ~ 59)");
         ageRangeList.add("50대 초반 (50 ~ 52)");
         ageRangeList.add("50대 중반 (53 ~ 56)");
         ageRangeList.add("50대 후반 (57 ~ 59)");
+        ageRangeList.add("60대 전체 (60 ~ 69)");
         ageRangeList.add("60대 초반 (60 ~ 62)");
         ageRangeList.add("60대 중반 (63 ~ 66)");
         ageRangeList.add("60대 후반 (67 ~ 69)");
+        ageRangeList.add("70대 전체 (70 ~ 79)");
         ageRangeList.add("70대 초반 (70 ~ 72)");
         ageRangeList.add("70대 중반 (73 ~ 76)");
         ageRangeList.add("70대 후반 (77 ~ 79)");
+        ageRangeList.add("80대 전체 (80 ~ 89)");
         ageRangeList.add("80대 초반 (80 ~ 82)");
         ageRangeList.add("80대 중반 (83 ~ 86)");
         ageRangeList.add("80대 후반 (87 ~ 89)");
+        ageRangeList.add("90대 전체 (80 ~ 99)");
         ageRangeList.add("90대 초반 (90 ~ 92)");
         ageRangeList.add("90대 중반 (93 ~ 96)");
         ageRangeList.add("90대 후반 (97 ~ 99)");
@@ -117,14 +122,15 @@ public class PollResultDialog extends DialogFragment {
         pollResultDialog_spinner_gender.setAdapter(genderRangeAdapter);
 
 
-
         pollResultDialog_spinner_gender.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 genderRange = genderList.get(position);
-                    Log.d("lkj in gd", genderRange);
-                    Log.d("lkj in gd_", ageRange);
+                Log.d("lkj in gd", genderRange);
+                Log.d("lkj in gd_", ageRange);
+                parsingData(genderRange, ageRange);
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
@@ -136,28 +142,16 @@ public class PollResultDialog extends DialogFragment {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 ageRange = ageRangeList.get(position);
-                    Log.d("lkj in ag", ageRange);
-                    Log.d("lkj in ag_", genderRange);
+                Log.d("lkj in ag", ageRange);
+                Log.d("lkj in ag_", genderRange);
+                parsingData(genderRange, ageRange);
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
             }
         });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         //차트클릭시 다이얼로그 닫기
         pollActivity_horizontalBarChart_result.setOnClickListener(new View.OnClickListener() {
@@ -166,7 +160,6 @@ public class PollResultDialog extends DialogFragment {
                 dismiss();
             }
         });
-
 
         Bundle bundle = this.getArguments();
         if (bundle != null) {
@@ -180,15 +173,17 @@ public class PollResultDialog extends DialogFragment {
             Log.d("lkj currentPick", String.valueOf(currentPick));
             Log.d("lkj currentHits", String.valueOf(contentHit));
             Log.d("lkj statisticsCodeeee", statisticsCode);
-            setChartData(imageN, contentKey, view, currentPick);
+
 //            getPicker(contentKey);            //textView Test
 
         }
-
-
         return view;
     }
 
+
+    /**
+     * onCreateView()     onCreateView()     onCreateView()     onCreateView()     onCreateView()     onCreateView()
+     */
 
     //투표한 사용자 uid parsing
     private void getPicker(String k) {
@@ -214,6 +209,7 @@ public class PollResultDialog extends DialogFragment {
                 Collections.addAll(stringArrayList, stringArray);
 
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
@@ -222,10 +218,8 @@ public class PollResultDialog extends DialogFragment {
     }
 
 
-
-
-    //차트 세팅
-    private void setChartData(final int contentN, String key, final View v, final int pick) {
+    //차트 초기 세팅 (전체 데이터)
+    private void setChartFullData(final int contentN, String key, final View v) {
 
         mDatabaseReference = FirebaseDatabase.getInstance().getReference();
         mDatabaseReference.child("user_contents").child(key).addListenerForSingleValueEvent(new ValueEventListener() {
@@ -263,6 +257,102 @@ public class PollResultDialog extends DialogFragment {
                 tmp.add(Integer.parseInt(object7.toString()));
                 tmp.add(Integer.parseInt(object8.toString()));
                 tmp.add(Integer.parseInt(object9.toString()));
+
+                HorizontalBarChart pollActivity_horizontalBarChart_result = v.findViewById(R.id.pollActivity_horizontalBarChart_result);
+//                HorizontalBarChart pollActivity_horizontalBarChart_result1 = v.findViewById(R.id.pollActivity_horizontalBarChart_result1);
+
+                CategoryBarChartXaxisFormatter xAxisFormatter = new CategoryBarChartXaxisFormatter(labels);
+                XAxis xAxis = pollActivity_horizontalBarChart_result.getXAxis();
+                xAxis.setValueFormatter(xAxisFormatter);
+                xAxis.setDrawAxisLine(false);
+                xAxis.setDrawGridLines(false);
+                xAxis.setGranularity(1);
+                xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+                xAxis.setXOffset(10); // 후보 1,2,3,4,5....위치
+                xAxis.setTextSize(20f); // 후보 1,2,3,4,5... 크기
+
+                YAxis yAxis = pollActivity_horizontalBarChart_result.getAxisLeft();
+                yAxis.setAxisMinimum(0);
+                yAxis.setMinWidth(0);
+                yAxis.setMaxWidth(3);
+                yAxis.setDrawZeroLine(true);
+                yAxis.setDrawTopYLabelEntry(true);
+
+                yAxis.setCenterAxisLabels(true);
+                yAxis.setEnabled(true);
+
+//                pollActivity_horizontalBarChart_result.getDescription().setEnabled(false);
+                pollActivity_horizontalBarChart_result.setTouchEnabled(true);
+                pollActivity_horizontalBarChart_result.setDragEnabled(false);
+                pollActivity_horizontalBarChart_result.setDoubleTapToZoomEnabled(false);
+                pollActivity_horizontalBarChart_result.setPinchZoom(false);
+                pollActivity_horizontalBarChart_result.setDescription(null);
+                pollActivity_horizontalBarChart_result.animateY(2500);
+                pollActivity_horizontalBarChart_result.setFitBars(true);
+                pollActivity_horizontalBarChart_result.setDrawBarShadow(false);
+                pollActivity_horizontalBarChart_result.getAxisLeft().setEnabled(false);
+                pollActivity_horizontalBarChart_result.getAxisRight().setEnabled(false);
+                pollActivity_horizontalBarChart_result.getXAxis().setEnabled(true);
+                pollActivity_horizontalBarChart_result.setDrawValueAboveBar(true);
+                pollActivity_horizontalBarChart_result.setDrawGridBackground(false);
+                pollActivity_horizontalBarChart_result.getLegend().setEnabled(false);
+
+
+                for (int i = 0; i < contentN; i++) {
+                    yValue.add(new BarEntry((float) contentN - i, tmp.get(i)));
+                }
+
+                BarDataSet set1 = new BarDataSet(yValue, null);
+//                set1.setColor(Color.GRAY);
+//                set1.setColors(ColorTemplate.MATERIAL_COLORS);
+                set1.setColors(0xff4485c9);
+
+                BarData data1 = new BarData(set1);
+                data1.setBarWidth(0.5f); //바 크기
+                data1.setValueTextSize(15f); //결과값 크기
+                data1.setValueTextColor(Color.GRAY);
+
+
+                ResultValueFormatter resultValueFormatter = new ResultValueFormatter();
+                data1.setValueFormatter(resultValueFormatter);
+
+                pollActivity_horizontalBarChart_result.setData(data1);
+                pollActivity_horizontalBarChart_result.invalidate(); //refresh
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("lkj setChartData", "setChartData ERR");
+
+            }
+        });
+
+    }
+
+
+    //차트 초기 세팅 (전체 데이터)
+    private void setChartPartData(final int contentN, String key, final View v, final int[] integerArrayList) {
+
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        mDatabaseReference.child("user_contents").child(key).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Map<String, Object> contentDTO = (Map<String, Object>) dataSnapshot.getValue();
+
+                ArrayList<String> labels = new ArrayList<>();
+                ArrayList<BarEntry> yValue = new ArrayList<>();
+                ArrayList<Integer> tmp = new ArrayList<>();
+
+                for (int i = 0; i < contentN + 1; i++) {
+                    int j = contentN - i + 1;
+                    labels.add("`" + String.valueOf(j));
+                }
+
+                for (int i = 0; i < contentN; i++) {
+                    tmp.add(integerArrayList[i]);
+                }
 
                 HorizontalBarChart pollActivity_horizontalBarChart_result = v.findViewById(R.id.pollActivity_horizontalBarChart_result);
 //                HorizontalBarChart pollActivity_horizontalBarChart_result1 = v.findViewById(R.id.pollActivity_horizontalBarChart_result1);
@@ -327,7 +417,6 @@ public class PollResultDialog extends DialogFragment {
                 pollActivity_horizontalBarChart_result.setData(data1);
                 pollActivity_horizontalBarChart_result.invalidate(); //refresh
 
-
             }
 
             @Override
@@ -336,7 +425,197 @@ public class PollResultDialog extends DialogFragment {
 
             }
         });
+    }
 
+
+    private void parsingData(String gR, String aR) {
+        String[] stringArray = null;
+        stringArray = statisticsCode.split(":");
+        int[] tmpStatistics = new int[stringArray.length];
+        for (int i = 0; i < stringArray.length; i++) {
+            tmpStatistics[i] = Integer.parseInt(stringArray[i]);
+        }
+
+        if (gR.equals("전 체") && aR.equals("전 체")) {
+            setChartFullData(imageN, contentKey, getView());
+            Log.d("lkj parsingData", "초기상태"); //처음 onCreate에서 차트 만들지 말고 여기서 한번만 만들면 될듯
+            //10대전체, 20대전체 만들고 선택시 초중후 합치는 로직으로?
+        } else {
+            if (gR.equals("남 자")) {
+                if(aR.equals("전 체")) {
+                    int[] tmpStatistics_male = new int[10];
+                    System.arraycopy(tmpStatistics,10, tmpStatistics_male, 0, 10);
+                    setChartPartData(imageN, contentKey, getView(), tmpStatistics_male);
+                } else {
+                    switch (aR) {
+                        case "10대 초반 (10 ~ 12)" :
+                            break;
+                        case "10대 중반 (13 ~ 16)" :
+                            break;
+                        case "10대 후반 (17 ~ 19)" :
+                            break;
+                        case "20대 초반 (20 ~ 22)" :
+                            break;
+                        case "20대 중반 (23 ~ 26)" :
+                            break;
+                        case "20대 후반 (27 ~ 20)" :
+                            break;
+                        case "30대 초반 (30 ~ 32)" :
+                            break;
+                        case "30대 중반 (33 ~ 36)" :
+                            break;
+                        case "30대 후반 (37 ~ 39)" :
+                            break;
+                        case "40대 초반 (40 ~ 42)" :
+                            break;
+                        case "40대 중반 (43 ~ 46)" :
+                            break;
+                        case "40대 후반 (47 ~ 40)" :
+                            break;
+                        case "50대 초반 (50 ~ 52)" :
+                            break;
+                        case "50대 중반 (53 ~ 56)" :
+                            break;
+                        case "50대 후반 (57 ~ 59)" :
+                            break;
+                        case "60대 초반 (60 ~ 62)" :
+                            break;
+                        case "60대 중반 (63 ~ 66)" :
+                            break;
+                        case "60대 후반 (67 ~ 69)" :
+                            break;
+                        case "70대 초반 (70 ~ 72)" :
+                            break;
+                        case "70대 중반 (73 ~ 76)" :
+                            break;
+                        case "70대 후반 (77 ~ 79)" :
+                            break;
+                        case "80대 초반 (80 ~ 82)" :
+                            break;
+                        case "80대 중반 (83 ~ 86)" :
+                            break;
+                        case "80대 후반 (87 ~ 89)" :
+                            break;
+                        case "90대 초반 (90 ~ 92)" :
+                            break;
+                        case "90대 중반 (93 ~ 96)" :
+                            break;
+                        case "90대 후반 (97 ~ 99)" :
+                            break;
+                        case "10대 전체 (10 ~ 19)" :
+                            break;
+                        case "20대 전체 (20 ~ 29)" :
+                            break;
+                        case "30대 전체 (30 ~ 39)" :
+                            break;
+                        case "40대 전체 (40 ~ 49)" :
+                            break;
+                        case "50대 전체 (50 ~ 59)" :
+                            break;
+                        case "60대 전체 (60 ~ 69)" :
+                            break;
+                        case "70대 전체 (70 ~ 79)" :
+                            break;
+                        case "80대 전체 (80 ~ 89)" :
+                            break;
+                        case "90대 전체 (80 ~ 99)" :
+                            break;
+
+
+                    }
+
+                }
+
+            }
+            if (gR.equals("여 자")) {
+                if (aR.equals("전 체")) {
+                    int[] tmpStatistics_female = new int[10];
+                    System.arraycopy(tmpStatistics,0, tmpStatistics_female, 0, 10);
+                    setChartPartData(imageN, contentKey, getView(), tmpStatistics_female);
+                } else {
+                    switch (aR) {
+
+                        case "10대 초반 (10 ~ 12)" :
+                            break;
+                        case "10대 중반 (13 ~ 16)" :
+                            break;
+                        case "10대 후반 (17 ~ 19)" :
+                            break;
+                        case "20대 초반 (20 ~ 22)" :
+                            break;
+                        case "20대 중반 (23 ~ 26)" :
+                            break;
+                        case "20대 후반 (27 ~ 20)" :
+                            break;
+                        case "30대 초반 (30 ~ 32)" :
+                            break;
+                        case "30대 중반 (33 ~ 36)" :
+                            break;
+                        case "30대 후반 (37 ~ 39)" :
+                            break;
+                        case "40대 초반 (40 ~ 42)" :
+                            break;
+                        case "40대 중반 (43 ~ 46)" :
+                            break;
+                        case "40대 후반 (47 ~ 40)" :
+                            break;
+                        case "50대 초반 (50 ~ 52)" :
+                            break;
+                        case "50대 중반 (53 ~ 56)" :
+                            break;
+                        case "50대 후반 (57 ~ 59)" :
+                            break;
+                        case "60대 초반 (60 ~ 62)" :
+                            break;
+                        case "60대 중반 (63 ~ 66)" :
+                            break;
+                        case "60대 후반 (67 ~ 69)" :
+                            break;
+                        case "70대 초반 (70 ~ 72)" :
+                            break;
+                        case "70대 중반 (73 ~ 76)" :
+                            break;
+                        case "70대 후반 (77 ~ 79)" :
+                            break;
+                        case "80대 초반 (80 ~ 82)" :
+                            break;
+                        case "80대 중반 (83 ~ 86)" :
+                            break;
+                        case "80대 후반 (87 ~ 89)" :
+                            break;
+                        case "90대 초반 (90 ~ 92)" :
+                            break;
+                        case "90대 중반 (93 ~ 96)" :
+                            break;
+                        case "90대 후반 (97 ~ 99)" :
+                            break;
+                        case "10대 전체 (10 ~ 19)" :
+                            break;
+                        case "20대 전체 (20 ~ 29)" :
+                            break;
+                        case "30대 전체 (30 ~ 39)" :
+                            break;
+                        case "40대 전체 (40 ~ 49)" :
+                            break;
+                        case "50대 전체 (50 ~ 59)" :
+                            break;
+                        case "60대 전체 (60 ~ 69)" :
+                            break;
+                        case "70대 전체 (70 ~ 79)" :
+                            break;
+                        case "80대 전체 (80 ~ 89)" :
+                            break;
+                        case "90대 전체 (80 ~ 99)" :
+                            break;
+                    }
+
+                }
+
+            }
+
+            Log.d("lkj parsingData gR", gR);
+            Log.d("lkj parsingData aR", aR);
+        }
     }
 
 
