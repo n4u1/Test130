1cbaba8497b7dfec40d8fdc0c480b854c46fd4bc
n4u1 <lkj840211@gmail.com>
2018-09-07 03:03:40 +0900

    20180907_0303_searching_complete

--- app/src/main/java/com/example/n4u1/test130/views/SearchHomeActivity.java
+++ app/src/main/java/com/example/n4u1/test130/views/SearchHomeActivity.java
@@ -1,10 +1,13 @@
 package com.example.n4u1.test130.views;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,17 +16,36 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.n4u1.test130.R;
+import com.example.n4u1.test130.models.ContentDTO;
 import com.example.n4u1.test130.util.ListViewSearchCategoryAdapter;
+import com.example.n4u1.test130.util.ListViewSearchTitleAdapter;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class SearchHomeActivity extends AppCompatActivity {
 
 
-    private ListView listView;
-    private ArrayList<String> stringArrayList;
-    private ListViewSearchCategoryAdapter adapter;
-    private SearchView searchView;
+
+
+    private ListView listViewCategory;
+    private ListView listViewTitle;
+    private ArrayList<String> stringArrayCategoryList;
+    private ArrayList<String> stringArrayTitleList;
+
+
+    private ListViewSearchCategoryAdapter adapterCategory;
+    private ListViewSearchTitleAdapter adapterTitle;
+    FirebaseDatabase mFirebaseDatabase;
+    DatabaseReference mDatabaseReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,20 +54,65 @@ public class SearchHomeActivity extends AppCompatActivity {
 
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
-        setCatogoryData();
+
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle(" ");
         }
         getSupportActionBar().setIcon(R.mipmap.ic_q_custom);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        listView = findViewById(R.id.searchHomeActivity_listView_category);
-        adapter = new ListViewSearchCategoryAdapter(this, R.layout.search_item_listview, stringArrayList);
-        listView.setAdapter(adapter);
+        listViewCategory = findViewById(R.id.searchHomeActivity_listView_category);
+        listViewTitle = findViewById(R.id.searchHomeActivity_listView_title);
 
 
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//        https://stackoverflow.com/questions/31966453/firebase-where-to-call-addchildeventlistener-so-that-i-can-save-bandwidth
+//        위의 주소에서처럼 많은량의 user_contents가 누적되었을경우 SharedPreferences(?) 를 이용해서 이어서 검색하게함
+//        Query newItems = mDatabaseReference.orderByKey().startAt(lastChildKey);
+        //검색 대상인 title을 arraylist로 뽑아서 세팅
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("user_contents");
+        mDatabaseReference.orderByChild("title").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                ArrayList<String> tmp = new ArrayList<>();
+
+                Iterator<DataSnapshot> contentDTOIterator = dataSnapshot.getChildren().iterator();
+
+                while (contentDTOIterator.hasNext()) {
+                    final ContentDTO contentDTO = contentDTOIterator.next().getValue(ContentDTO.class);
+                    tmp.add(contentDTO.title);
+                }
+                setTitleData(tmp);
+                setCategoryData();
+
+                adapterCategory = new ListViewSearchCategoryAdapter(SearchHomeActivity.this, R.layout.search_item_listview, stringArrayCategoryList);
+                adapterTitle = new ListViewSearchTitleAdapter(SearchHomeActivity.this, R.layout.search_item_title_listview, stringArrayTitleList);
+                listViewCategory.setAdapter(adapterCategory);
+                listViewTitle.setAdapter(adapterTitle);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
+
+
+        listViewCategory.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(SearchHomeActivity.this, (String)parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+        listViewTitle.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(SearchHomeActivity.this, (String)parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
@@ -58,34 +125,45 @@ public class SearchHomeActivity extends AppCompatActivity {
 
     }
 
-    private void setCatogoryData() {
-        stringArrayList = new ArrayList<>();
-        stringArrayList.add("아침");
-        stringArrayList.add("아무거나");
-        stringArrayList.add("아거");
-        stringArrayList.add("무거나");
-        stringArrayList.add("점심");
-        stringArrayList.add("심심무");
-        stringArrayList.add("침거");
-        stringArrayList.add("무사히");
-        stringArrayList.add("사사로아");
-        stringArrayList.add("이경준");
-        stringArrayList.add("아,무,거,나");
-        stringArrayList.add("야구");
-        stringArrayList.add("농구");
-        stringArrayList.add("축구");
-        stringArrayList.add("축지법");
-        stringArrayList.add("대한민국 헌법");
-        stringArrayList.add("성셋별");
-        stringArrayList.add("별하나");
-        stringArrayList.add("대,한.민;국");
-        stringArrayList.add("아니");
-        stringArrayList.add("아니아니");
-        stringArrayList.add("아니아니아아아아아");
+
+    //게시글 제목 검색결과 list
+    private void setTitleData(ArrayList<String> tmpString) {
+        stringArrayTitleList = new ArrayList<>();
+        stringArrayTitleList.addAll(tmpString);
     }
+
+
+    //카테고리 검색결과 list
+    private void setCategoryData() {
+        stringArrayCategoryList = new ArrayList<>();
+        stringArrayCategoryList.add("아침");
+        stringArrayCategoryList.add("아무거나");
+        stringArrayCategoryList.add("아거");
+        stringArrayCategoryList.add("무거나");
+        stringArrayCategoryList.add("점심");
+        stringArrayCategoryList.add("심심무");
+        stringArrayCategoryList.add("침거");
+        stringArrayCategoryList.add("무사히");
+        stringArrayCategoryList.add("사사로아");
+        stringArrayCategoryList.add("이경준");
+        stringArrayCategoryList.add("아,무,거,나");
+        stringArrayCategoryList.add("야구");
+        stringArrayCategoryList.add("농구");
+        stringArrayCategoryList.add("축구");
+        stringArrayCategoryList.add("축지법");
+        stringArrayCategoryList.add("대한민국 헌법");
+        stringArrayCategoryList.add("성셋별");
+        stringArrayCategoryList.add("별하나");
+        stringArrayCategoryList.add("대,한.민;국");
+        stringArrayCategoryList.add("아니");
+        stringArrayCategoryList.add("아니아니");
+        stringArrayCategoryList.add("아니아니아아아아아");
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        SearchView searchView;
         getMenuInflater().inflate(R.menu.home_search_menu, menu);
         MenuItem myActionMenuItem = menu.findItem(R.id.menu_search);
         searchView = (SearchView) myActionMenuItem.getActionView();
@@ -100,10 +178,13 @@ public class SearchHomeActivity extends AppCompatActivity {
             @Override
             public boolean onQueryTextChange(String newText) {
                 if (TextUtils.isEmpty(newText)) {
-                    adapter.filter("");
-                    listView.clearTextFilter();
+                    adapterCategory.filter("");
+                    adapterTitle.filter("");
+                    listViewCategory.clearTextFilter();
+                    listViewTitle.clearTextFilter();
                 } else {
-                    adapter.filter(newText);
+                    adapterTitle.filter(newText);
+                    adapterCategory.filter(newText);
                 }
                 return true;
             }
